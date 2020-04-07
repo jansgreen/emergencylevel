522aa92c44e62802fbb958573438112e3c464d22
Luis Pena <Luis Pena>
2020-04-07 17:27:47 -0400

    get mongodb json ready

--- run.py
+++ run.py
@@ -3,15 +3,19 @@ from pymongo import MongoClient
 from flask import Flask, render_template, request, redirect, url_for, flash
 from datetime import datetime
 from datetime import timedelta
+from flask_material import Material
+
 
 app = Flask(__name__)
+Material(app)
 
 # MONGODB CONNECTION
 client = pymongo.MongoClient(
     "mongodb+srv://jansgreen:Lmongogreen07@cluster0-ajilk.mongodb.net/test?retryWrites=true&w=majority")
 db = client["userRecord"]
 dbColl = db["userRecord"]
-print(db.list_collection_names)
+
+
 
 # SETTING
 app.secret_key = 'mysecretkey'
@@ -51,21 +55,34 @@ def addPatient():
             "BOD": BOD,
             "Address": Address,
             "Email": Email,
-            "numPhone": numPhone,
+            "Phone": numPhone,
             "Condition": Condition,
-            "ntCondition": ntCondition
+            "Note": ntCondition
         }
     dbColl.insert_one(dataPost)
     now = datetime.now()
     ticketNum = now.strftime('%d%m%YE%H%M%S')
     flash(ticketNum)
-    flash("Name: " +name + lastName)
+    flash("Name: " + name +" "+lastName)
     return redirect(url_for("ticket"))
 
+
+@app.route('/doctorList')
+def doctorList():
+    DoctorsList = dbColl.find({"Category":"staff"})
+    for DRlistDB in DoctorsList:
+        flash(DRlistDB['First Name'] +" "+ DRlistDB['Last Name']+ " " + DRlistDB['BOD'] +" "+ DRlistDB['Email']+ " " + DRlistDB['Phone']+ " " + DRlistDB['language'] + " "+ DRlistDB['Position'] +" "+ DRlistDB['Tab number'])
+    return render_template("doctorList.html")
 
 @app.route('/staff')
 def staff():
     return render_template("staff.html")
+
+
+@app.route('/emergencyTeam')
+def emergencyTeam():
+     return render_template("emergencyTeam.html")
+
 
 
 @app.route('/board')
@@ -82,8 +99,10 @@ def addStaff():
         Address = request.form['address']
         Email = request.form['email']
         numPhone = request.form['icon_telephone']
+        language = request.form['language']
+        Tabnumber = request.form.get_json('TabDoctors')
         rooms = request.form['rooms']
-        Position = request.form['Position']
+        Position = request.form('Position')  #request.form.to_dict() USALO PARA ASIGNAR PERSONAL A LOS ROOMS
         Category = "staff"
         dataPost = {
             "Category": Category,
@@ -92,19 +111,19 @@ def addStaff():
             "BOD": BOD,
             "Address": Address,
             "Email": Email,
-            "numPhone": numPhone,
+            "Phone": numPhone,
+            "language":language,
+            "Tab number": Tabnumber,
             "rooms": rooms,
             "Position": Position
         }
     dbColl.insert_one(dataPost)
     return render_template("staff.html")
 
+
 @app.route('/room')
 def room():
     return render_template("room.html")
-
-
-#JS
 
 
 if __name__ == '__main__':
--- static/scripts/script.js
+++ static/scripts/script.js
@@ -1,5 +1,63 @@
-function saludo() {
-  var indexContainers = document.getElementById("indexContainer")
-indexContainers.innerHTML = 'Hola Luis';
-  
+/*====================================================
+    APPOINTMENTS
+====================================================*/
+window.onload = function () {
+  var contador = 0;
+  const botones=document.querySelectorAll("#generate");
+  botones.forEach(el => {
+      el.addEventListener("click", contar);
+  });
+
+  function contar()
+  {
+      contador++;
+      document.getElementById("TabDoctors").innerHTML = `${contador}`;
+  }
 }
+    
+
+/*
+
+(function() {
+	 function IDGenerator() {
+	 
+		 this.length = 8;
+		 this.timestamp = +new Date;
+		 
+		 var _getRandomInt = function( min, max ) {
+			return Math.floor( Math.random() * ( max - min + 1 ) ) + min;
+		 }
+		 
+		 this.generate = function() {
+			 var ts = this.timestamp.toString();
+			 var parts = ts.split( "" ).reverse();
+			 var id = "";
+			 
+			 for( var i = 0; i < this.length; ++i ) {
+				var index = _getRandomInt( 0, parts.length - 1 );
+				id += parts[index];	 
+			 }
+			 
+			 return id;
+		 }
+
+		 
+	 }
+	 
+	 
+	 document.addEventListener( "DOMContentLoaded", function() {
+		var btn = document.querySelector( "#generate" ),
+			output = document.querySelector( "#output" );
+			
+		btn.addEventListener( "click", function() {
+			var generator = new IDGenerator();
+			output.innerHTML = generator.generate();
+			
+		}, false); 
+		 
+	 });
+	 
+	 
+ })();
+
+*/
